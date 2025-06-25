968c3d3fb1164aab70850488b6f25e47daa02c97
tofashish527 <tofashish000@gmail.com>
2025-06-25 10:55:41 +0600

    delete booking added

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
         const result=await bookingCollection.insertOne(booking);
         res.send(result);
     })
+
+    app.delete('/booking/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await bookingCollection.deleteOne(query);
+  res.send(result);
+});
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
